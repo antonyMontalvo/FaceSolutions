da67b071dd99908d5435c1a013d51197e3077fe3
RobertoUnocc <carlitos2017unocc@gmail.com>
2020-07-17 16:48:42 -0500

    Subiendo el login y registro de Administrador

--- desarrollo/FRSIAAATR/Codificación/frsiaaatr_admin/src/routes/admin.routes.js
+++ desarrollo/FRSIAAATR/Codificación/frsiaaatr_admin/src/routes/admin.routes.js
@@ -3,5 +3,11 @@ const router = require("express").Router();
 router.get("/", (req, res) => {
   res.render("admin/index");
 });
+router.get("/login", (req, res) => {
+  res.render("prueba",{layout:"login"});
+});
+router.get("/registro", (req, res) => {
+  res.render("prueba",{layout:"registro"});
+});
 
 module.exports = router;

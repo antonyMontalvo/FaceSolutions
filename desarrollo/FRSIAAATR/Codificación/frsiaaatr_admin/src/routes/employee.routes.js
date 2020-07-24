d75d2b33ec65d47a3b0c8cbd27c8f40e0cc66d24
Christian <arturogarcia_96@hotmail.com>
2020-07-24 16:22:39 -0500

    feat: obtener todos los procesos en curso

--- desarrollo/FRSIAAATR/Codificación/frsiaaatr_admin/src/routes/employee.routes.js
+++ desarrollo/FRSIAAATR/Codificación/frsiaaatr_admin/src/routes/employee.routes.js
@@ -1,12 +1,11 @@
 const router = require("express").Router();
 
 const employeeController = require("../controllers/employee.controller"),
-  validator = require("../middlewares/validation"),
-  authenticate = require("../middlewares/authenticate"),
-{ checkToken } = require("../middlewares/auth");
+    validator = require("../middlewares/validation"),
+    authenticate = require("../middlewares/authenticate"),
+    { checkToken } = require("../middlewares/auth");
 
 router
-    .get("/index", checkToken, employeeController.getEmployees)
-    ;
-
+    .get("/index", /*checkToken,*/ employeeController.getEmployees)
+    .get("/process", /*checkToken,*/ employeeController.getAllProcess);
 module.exports = router;

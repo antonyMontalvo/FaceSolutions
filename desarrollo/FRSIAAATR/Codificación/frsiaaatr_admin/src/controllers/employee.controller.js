1de0d1206817d293c6afb4cd4e4a24ab33c959c1
Christian <arturogarcia_96@hotmail.com>
2020-08-07 16:40:19 -0500

    fix: DataTable y Filtros de búsqueda de Solicitudes no Leídas

--- desarrollo/FRSIAAATR/Codificación/frsiaaatr_admin/src/controllers/employee.controller.js
+++ desarrollo/FRSIAAATR/Codificación/frsiaaatr_admin/src/controllers/employee.controller.js
@@ -8,7 +8,7 @@ const Department = require("../models/department"), //pongo Employee porque es u
 
 const { QueryTypes } = require('sequelize');
 const { sequelizeDB } = require("../../config/database");
-  
+
 EmployeesController.getEmployees = async(req, res) => {
     try {
         // const employees = await Department.findAll();
@@ -29,10 +29,35 @@ EmployeesController.getEmployees = async(req, res) => {
 EmployeesController.getAllProcess = async(req, res) => {
     try {
 
-        const q = 'SELECT p.code, p.date_created, p.state, ps.name, ps.last_name_1, ps.last_name_2, c.name FROM process p LEFT JOIN postulant ps ON p.postulant_id = ps.id LEFT JOIN constancy c ON p.constancy_id = c.id';
+        const q = `SELECT 
+		p.code as codigoSolicitud,
+		p.state_process as estadoSolicitud,
+		pst.state_name as nombreEstadoSolicitud,  
+		p.date_created as fechaSolicitud,
+		c.name as nombreConstancia,
+		ps.name as nombre, 
+		ps.last_name_1 as apellidoPaterno, 
+		ps.last_name_2 as apellidoMaterno,
+		concat_ws(' ', ps.name, ps.last_name_1, ps.last_name_2) as nombreCompleto,  
+		ps.postulant_code as codigoPostulante,
+		ps.dni as numeroDocumento,
+		sp.name as escuelaAcademica,
+        sp.initials as escuelaIniciales,
+		f.name as facultad,
+        f.initials as facultadIniciales
+		FROM 
+		process p LEFT JOIN postulant ps 
+		ON p.postulant_id = ps.id 
+		LEFT JOIN constancy c 
+		ON p.constancy_id = c.id
+		LEFT JOIN specialty sp
+		ON ps.specialty_id = sp.id
+		LEFT JOIN faculty f
+		ON sp.faculty_id = f.id
+		LEFT JOIN process_state pst
+		ON p.state_process = pst.idprocess_state`;
 
         const process = await sequelizeDB.query(q);
-        //console.log(process);
         res.send(process[0]);
 
     } catch (error) {
@@ -42,7 +67,7 @@ EmployeesController.getAllProcess = async(req, res) => {
 };
 
 EmployeesController.generatePdf = async(req, res) => {
-    
+
     const path = require("path");
     const puppeteer = require("puppeteer");
     const handlebars = require("handlebars");
@@ -55,34 +80,35 @@ EmployeesController.generatePdf = async(req, res) => {
     app.engine('hbs', hbs({ extname: 'hbs' }));
     app.set('view engine', 'hbs');
     app.use(express.static(path.join(__dirname, 'public')));
-    (async () => {
-    try{
-    
-    let browser = null;
+    (async() => {
+        try {
 
-    const file = fs.readFileSync('./src/template/constancy.html', 'utf8');
-    const template = handlebars.compile(file);
-    const html = template({name:"Evelin Sofía Pariona Gutierrez"});
-     
-    browser = await puppeteer.launch({
-        pipe: true,
-        args: ['--headless', '--disable-gpu', '--full-memory-crash-report', '--unlimited-storage',
-               '--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
-    });
-    
-    const page = await browser.newPage();
-    await page.setContent(html);
-    await page.pdf({ path: "./pdf/constancy.pdf", format: "Letter" });
-    await browser.close();
-    
-    res.send(200);
+            let browser = null;
 
-    }catch(error){
-        console.log(error);
-    }
+            const file = fs.readFileSync('./src/template/constancy.html', 'utf8');
+            const template = handlebars.compile(file);
+            const html = template({ name: "Evelin Sofía Pariona Gutierrez" });
+
+            browser = await puppeteer.launch({
+                pipe: true,
+                args: ['--headless', '--disable-gpu', '--full-memory-crash-report', '--unlimited-storage',
+                    '--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'
+                ],
+            });
+
+            const page = await browser.newPage();
+            await page.setContent(html);
+            await page.pdf({ path: "./pdf/constancy.pdf", format: "Letter" });
+            await browser.close();
+
+            res.send(200);
+
+        } catch (error) {
+            console.log(error);
+        }
     })();
 
-    
+
 };
 
 module.exports = EmployeesController;

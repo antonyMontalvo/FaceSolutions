628648c74a45b9133ef934e722b42c198d3e317c
Antony Montalvo <antonis162010@gmail.com>
2020-08-11 11:24:35 -0500

    Primera fase implementación reconocimiento facial

--- desarrollo/FRSIAAATR/Codificación/frsiaaatr_postulant/src/public/api/face.js
+++ desarrollo/FRSIAAATR/Codificación/frsiaaatr_postulant/src/public/api/face.js
@@ -156,7 +156,7 @@ function loadLabeledImages(dato) {
     return Promise.all(
         labels.map(async label => {
             const descriptions = []
-            for (let i = 1; i <= 4; i++) {
+            for (let i = 1; i <= 2; i++) {
                 const img = await faceapi.fetchImage(`/perfiles/${label}/${i}.jpg`);
                 const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
                 descriptions.push(detections.descriptor);

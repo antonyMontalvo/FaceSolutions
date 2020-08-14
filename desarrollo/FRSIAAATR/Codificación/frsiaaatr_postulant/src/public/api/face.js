0605ae7bd7951ab34fa902c6d4b458b564686c7d
Antony Montalvo <antonis162010@gmail.com>
2020-08-14 15:35:43 -0500

    Validaciones para el scanner

--- desarrollo/FRSIAAATR/Codificación/frsiaaatr_postulant/src/public/api/face.js
+++ desarrollo/FRSIAAATR/Codificación/frsiaaatr_postulant/src/public/api/face.js
@@ -156,7 +156,7 @@ function loadLabeledImages(dato) {
     return Promise.all(
         labels.map(async label => {
             const descriptions = []
-            for (let i = 1; i <= 2; i++) {
+            for (let i = 1; i <= 10; i++) {
                 const img = await faceapi.fetchImage(`/perfiles/${label}/${i}.jpg`);
                 const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
                 descriptions.push(detections.descriptor);

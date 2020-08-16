7723d411fa4e530a0373a543f13cf931225a93ad
Ricardo <tricardo003@gmail.com>
2020-08-15 23:58:48 -0500

    update views

--- desarrollo/FRSIAAATR/Codificación/frsiaaatr_postulant/src/controllers/postulant.controller.js
+++ desarrollo/FRSIAAATR/Codificación/frsiaaatr_postulant/src/controllers/postulant.controller.js
@@ -1,64 +1,20 @@
 const bcrypt = require("bcryptjs");
-<<<<<<< HEAD
-const Department = require("../models/department"), //pongo Employee porque es un modelo
+
+const Postulant = require("../models/postulant"),
   { createToken, getPayload } = require("../services/jwt"),
-  PostulantController = {},
+  EmployeesController = {},
   saltRounds = 10;
-PostulantController.getIndex = async (req, res) => {
+
+EmployeesController.login = async (req, res) => {
   try {
-    res.render("postulant/index");
+    const postulants = await Postulant.findAll();
+    console.log(postulants);
+
+    res.render("login", { layout: null });
   } catch (error) {
     console.log(error.stack);
     return res.status(500).json({ error: error.stack });
   }
 };
 
-PostulantController.getLogin = async (req, res) => {
-  try {
-    res.render("login", { layout: null });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ error: error.stack });
-  }
-};
-
-PostulantController.getRegistre = async (req, res) => {
-  try {
-    res.render("registro", { layout: null });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ error: error.stack });
-  }
-};
-
-PostulantController.getRegistrePhoto = async (req, res) => {
-  try {
-    res.render("registroFotos", { layout: null });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ error: error.stack });
-  }
-};
-
-module.exports = PostulantController;
-=======
-
-const Postulant = require("../models/postulant"),
-    {createToken, getPayload} = require("../services/jwt"),
-    EmployeesController = {},
-    saltRounds = 10;
-
-EmployeesController.login = async (req, res) => {
-    try {
-        const postulants = await Postulant.findAll();
-        console.log(postulants);
-
-        res.render("login", {layout: null});
-    } catch (error) {
-        console.log(error.stack);
-        return res.status(500).json({error: error.stack});
-    }
-};
-
 module.exports = EmployeesController;
->>>>>>> 8e29c51194a8209341e1129bea6fa0951c6c2147
--- desarrollo/FRSIAAATR/Codificación/frsiaaatr_postulant/src/routes/index.js
+++ desarrollo/FRSIAAATR/Codificación/frsiaaatr_postulant/src/routes/index.js
@@ -2,20 +2,12 @@ const indexRouter = {};
 
 const Routes = {
   employeeRouter: require("./employee.routes"),
-<<<<<<< HEAD
-  postulantRouter: require("./postulant.routes"),
-=======
   postulantRouter: require("./postulant.route"),
->>>>>>> 8e29c51194a8209341e1129bea6fa0951c6c2147
 };
 
 indexRouter.getRoutes = (app) => {
   app.use(`${process.env.APP_URL}/employees`, Routes.employeeRouter); // No tocar ejemplo
-<<<<<<< HEAD
-  app.use(`${process.env.APP_URL}/`, Routes.postulantRouter);
-=======
   app.use(`${process.env.APP_URL}`, Routes.postulantRouter); // No tocar ejemplo
->>>>>>> 8e29c51194a8209341e1129bea6fa0951c6c2147
 };
 
 module.exports = indexRouter;

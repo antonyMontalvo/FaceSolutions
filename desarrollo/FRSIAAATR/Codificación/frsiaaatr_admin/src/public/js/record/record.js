9474cb5cac5575fc7d855c760a8ba5a02f3af61e
Christian <arturogarcia_96@hotmail.com>
2020-08-17 23:16:32 -0500

    feat: Vista historial y filtro checkbox funcionando

---
+++ desarrollo/FRSIAAATR/Codificación/frsiaaatr_admin/src/public/js/record/record.js
@@ -0,0 +1,144 @@
+$(document).ready(function() {
+
+    var $local = {
+        $tblHistorial: $("#tblHistorial"),
+        tblHistorial: ""
+    }
+
+    $local.tblHistorial = $local.$tblHistorial.DataTable({
+        "ajax": {
+            "url": "../../record/requests-all",
+            "dataSrc": "",
+            "beforeSend": function(xhr) {
+                xhr.setRequestHeader('Content-Type', 'application/json');
+            },
+        },
+        "initComplete": function() {
+            $local.$tblHistorial.wrap("<div class='table-responsive'></div>");
+            $(window).resize();
+        },
+        "columnDefs": [{
+            "targets": [0, 1, 2, 3],
+            "className": "all filtrable text-center",
+        }, ],
+        "columns": [{ // 0
+                "data": 'estadoSolicitud',
+                "title": "Estado",
+                "render": function(data, type, row) {
+                    var a = ' ';
+                    switch (row.estadoSolicitud) {
+                        case 1:
+                            a = '<span class="badge badge-flat border-info text-info-800">' + row.nombreEstadoSolicitud + '</span>';
+                            break;
+                        case 2:
+                            a = '<span class="badge badge-flat border-warning text-warning-800">' + row.nombreEstadoSolicitud + '</span>';
+                            break;
+                        default:
+                            a = '<span class="badge badge-flat border-dark text-dark-600">' + row.nombreEstadoSolicitud + '</span>';
+                    }
+                    return a;
+                }
+            },
+            { // 1
+                "data": 'codigoSolicitud',
+                "title": 'Código',
+                "render": function(data, type, row) {
+                    var code = '<span style="font-size: 100%; " class="badge badge-light badge-striped badge-striped-left border-left-primary">' + row.codigoSolicitud + '</span>'
+                    return code;
+                }
+            },
+            { // 2
+                "data": 'fechaSolicitud',
+                "title": 'Fecha Solicitud'
+            },
+            { // 3
+                "title": "Asunto",
+                "render": function(data, type, row) {
+                    var a = (row.nombreConstancia + " - " + row.facultadIniciales + " - Cod postulante: " +
+                        row.codigoPostulante + " - DNI: " + row.numeroDocumento).toUpperCase();
+                    var ref = '<a href="review-request/' + row.codigoSolicitud + '">' + a + '</a>';
+                    return ref;
+                }
+            }
+        ],
+
+    });
+
+    //Filtro checkbox
+    var estados = [];
+    var constancias = [];
+    var especialidades = [];
+
+    //BUSQUEDA FILTRO
+    $(':checkbox').change(function() {
+        if ($(this).is(":checked")) {
+            estados = []; //Limpiando el arreglo cada vez que das check, si no se repiten valores
+            $("#checkbox-container :checked").each(function() {
+                estados.push($(this).val());
+            });
+
+            constancias = [];
+            $("#checkbox-container-constancias :checked").each(function() {
+                constancias.push($(this).val());
+            });
+
+            especialidades = [];
+            $("#checkbox-container-especialidades :checked").each(function() {
+                especialidades.push($(this).val());
+            });
+
+        } else {
+            //Extrae el name del checkbox que ha cambiado a 'unchecked' / se ha desmarcado.
+            var name = $(this).attr('name');
+            var x = "";
+
+            switch (name) { //En base a este name, sabemos de que array vamos a quitar el valor que se ha desmarcado
+                case 'estados':
+                    x = estados.indexOf($(this).val());
+                    estados.splice(x, 1);
+                    break;
+                case 'constancias':
+                    x = constancias.indexOf($(this).val());
+                    constancias.splice(x, 1);
+                    break;
+                case 'destino':
+                    x = especialidades.indexOf($(this).val());
+                    especialidades.splice(x, 1);
+                    break;
+            }
+        }
+        console.log(estados);
+        var data = {
+            "idFacultad": 1,
+            "arrayEstados": estados,
+            "arrayConstancias": constancias,
+            "arrayEspecialidades": especialidades
+        }
+        console.log(data);
+
+        //Cada vez que se marca o desmarca un checkbox, se ejecuta este ajax según los arrays...
+        $.ajax({
+            type: "POST",
+            url: "../../record/requests-filter",
+            data: JSON.stringify(data),
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader('Content-Type', 'application/json');
+            }
+        }).done(function(response) {
+            console.log(response);
+
+            $local.tblHistorial.clear().draw();
+            $globalFunction.loadBlock($("#cardFinalizadas"), 'Actualizando información...', 900,
+                function() {
+                    $(this).parent().addClass("loading");
+                    $local.tblHistorial.rows.add(response).draw();
+                }
+            )
+
+        }).fail(function(xhr) {
+            console.log("Error");
+        });
+
+    });
+
+});
